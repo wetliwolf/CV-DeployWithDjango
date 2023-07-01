b60a1f4926658934fc8af6f38f481d4f7cefb10c
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-07-01 16:03:25 +0000

    bower version

---
+++ deployment/cv/views.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
