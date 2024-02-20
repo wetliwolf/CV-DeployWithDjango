40b284f7a72272350c4d3ad6ead17db8c9c80daf
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-02-20 12:00:35 +0000

    Updated versions to non snapshot

--- deployment/cv_vid/views.py
+++ deployment/cv_vid/views.py
@@ -1,2 +1,18 @@
 
 from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+
+import torch
+from torchvision import models
+import torchvision.transforms as T
+from PIL import Image
+import numpy as np
