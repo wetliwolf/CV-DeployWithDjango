79d917d1f025bf997d0fdbb6284406efd1dd6b45
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-02-23 22:37:31 +0000

    new layout

--- deployment/cv_vid/views.py
+++ deployment/cv_vid/views.py
@@ -53,3 +53,18 @@ def get_segmentation(input_image, model):
 
 
 label_colors = np.array([(0, 0, 0),  # 0=background
+              # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+              (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
+              # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+              (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
+              # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
+              (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
+              # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+              (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+
+def seg2rgb(preds):
+    colors = label_colors
+    colors = label_colors.astype("uint8")
+
+    # plot the semantic segmentation predictions of 21 classes in each color
+    rgb = Image.fromarray(preds.byte().cpu().numpy())#.resize(preds.shape)
