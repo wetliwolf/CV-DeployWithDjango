9bcfbd33b1c59bc4f95423fea48ad93336255145
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-03-03 08:34:50 +0000

    Review fixes.

--- deployment/cv_vid/views.py
+++ deployment/cv_vid/views.py
@@ -117,3 +117,18 @@ COCO_INSTANCE_CATEGORY_NAMES = [
     'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
     'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
     'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+def get_prediction(img_path, threshold):
+    img = Image.open(img_path) # Load the image
+    transform = T.Compose([T.ToTensor()]) # Defing PyTorch Transform
+    img = transform(img) # Apply the transform to the image
+    model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True).eval()
+    pred = model([img]) # Pass the image to the model
+    pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
+    pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
+    pred_score = list(pred[0]['scores'].detach().numpy())
+    pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
+    pred_boxes = pred_boxes[:pred_t+1]
+    pred_class = pred_class[:pred_t+1]
+    return pred_boxes, pred_class
