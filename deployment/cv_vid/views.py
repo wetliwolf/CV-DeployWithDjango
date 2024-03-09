c0fb19ecb4eb1a90bf2a9b6093d77e0a62e1a5f3
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-03-09 15:06:39 +0000

    ASoC: dapm: Use card_list during DAPM shutdown

--- deployment/cv_vid/views.py
+++ deployment/cv_vid/views.py
@@ -160,3 +160,6 @@ def object_detection(request):
         cv2.imwrite(obb_file, img)
 
         return render(request, 'cv/object_detection.html', {'original_img': img_file,
+                                                            'obb_img': '/media/obb_img.png'})
+        
+    return render(request, 'cv/object_detection.html')  
