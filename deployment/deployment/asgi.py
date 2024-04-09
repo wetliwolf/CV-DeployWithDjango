9ae50dcf943070edb82042fb037601c8795f0eb4
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-04-09 22:57:02 +0000

    Fix gcc warnings.

--- deployment/deployment/asgi.py
+++ deployment/deployment/asgi.py
@@ -4,4 +4,4 @@ ASGI config for deployment project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deplo
+https://docs.djangoproject.com/en/3.0/howto/deployment/asg
