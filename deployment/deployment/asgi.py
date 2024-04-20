8bcf964962886556ce8c0e984beda710b66e09de
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-04-20 19:57:26 +0000

    Preparing new release.

--- deployment/deployment/asgi.py
+++ deployment/deployment/asgi.py
@@ -11,4 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deployment.setti
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deployment.settings')
+
+application 
