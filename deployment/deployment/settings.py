6f13b8406368014e647bec195187385529316635
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-04-25 21:20:59 +0000

    Update CHANGELOG.md for v2.3.0 release [skip ci]

--- deployment/deployment/settings.py
+++ deployment/deployment/settings.py
@@ -15,3 +15,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '0ex3px)8r!m@d9ig3hj8*=(+&_8^-!d8f^%dj*a87n)wfop#sv'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
