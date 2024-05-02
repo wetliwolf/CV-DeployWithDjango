11d9c41d3215e9cadde8f495fd7e2957f925a90c
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-05-02 13:49:02 +0000

    DB/Misc:

--- deployment/deployment/settings.py
+++ deployment/deployment/settings.py
@@ -68,3 +68,19 @@ TEMPLATES = [
         },
     },
 ]
+
+WSGI_APPLICATION = 'deployment.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+# Password validation
