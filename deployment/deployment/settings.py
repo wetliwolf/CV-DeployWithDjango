05ded6dea53e9fc98af43601eee766a585416e2b
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-05-02 07:38:13 +0000

    ChangeLog commited by build_package.sh

--- deployment/deployment/settings.py
+++ deployment/deployment/settings.py
@@ -56,3 +56,15 @@ ROOT_URLCONF = 'deployment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'cv/templates'),os.path.join(BASE_DIR, 'cv_vid/templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
