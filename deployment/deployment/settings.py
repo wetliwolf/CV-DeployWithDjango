28c8f4002a54fb0edc1dec4b28192c8c37cdb09c
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-05-07 20:42:50 +0000

    Add LICENSE file

--- deployment/deployment/settings.py
+++ deployment/deployment/settings.py
@@ -114,3 +114,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
