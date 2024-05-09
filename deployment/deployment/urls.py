cb4135c4c1bf2af2a79bfcdcdd4d87547279a080
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-05-09 20:47:34 +0000

    100% code coverage

--- deployment/deployment/urls.py
+++ deployment/deployment/urls.py
@@ -12,3 +12,16 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('cv/', include('cv.urls')),
+    path('cv_vid/', include('cv_vid.urls')),
+    path('admin/', admin.site.urls),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
