255f42b2e959f9bba40e9c8a450a831310c5e79b
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-05-27 11:46:30 +0000

    mv file

--- deployment/deployment/wsgi.py
+++ deployment/deployment/wsgi.py
@@ -4,4 +4,4 @@ WSGI config for deployment project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoprojec
+https://docs.djangoproject.com/en/3.0/h
